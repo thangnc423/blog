2196854619f622f58ab1c537f5076f2526768a56
Thang Chau <thangnc@uci.edu>
2024-11-14 17:47:57 -0800

    added mongodb and set up schema + mongoose

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', (req, res) => {
+mongoose.connect('mongodb+srv://blog:qNz46U6h2QTaCrmR@cluster0.cco02.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+app.post('/register', async (req, res) => {
     const {username, password} = req.body;
-    res.json({requestData:{username, password}});
+    const userDoc = await User.create({username, password});
+    res.json(userDoc);
 });
 
 app.listen(4000);
+// mongodb+srv://blog:qNz46U6h2QTaCrmR@cluster0.cco02.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// qNz46U6h2QTaCrmR
