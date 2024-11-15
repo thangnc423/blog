12aecea6f4b680e2a81bb1c5afeac9dbe1ba77ee
Thang Chau <thangnc@uci.edu>
2024-11-14 16:46:24 -0800

    added login and register pages

--- client/mern-blog/src/pages/LoginPage.js
+++ client/mern-blog/src/pages/LoginPage.js
@@ -1,7 +1,10 @@
 export default function LoginPage() {
     return (
-        <div>
-            login page
-        </div>
+        <form className="login">
+            <h1>Login</h1>
+            <input type="text" placeholder="username"></input>
+            <input type="password" placeholder="password"></input>
+            <button>Login</button>
+        </form>
     );
 }
---
+++ client/mern-blog/src/pages/RegisterPage.js
@@ -0,0 +1,10 @@
+export default function RegisterPage() {
+    return(
+        <form className="register">
+            <h1>Register</h1>
+            <input type="text" placeholder="username"></input>
+            <input type="password" placeholder="password"></input>
+            <button>Register</button>
+        </form>
+    );
+}
