d953dca6fcb9713633b7c2a7617551073be611c3
Thang Chau <thangnc@uci.edu>
2024-11-14 19:08:18 -0800

    added user registration to mongo and pw encryption

--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcrypt');
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -11,8 +14,14 @@ mongoose.connect('mongodb+srv://blog:qNz46U6h2QTaCrmR@cluster0.cco02.mongodb.net
 
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
-    const userDoc = await User.create({username, password});
-    res.json(userDoc);
+    try{
+        const userDoc = await User.create({
+            username, 
+            password:bcrypt.hashSync(password, salt)});
+        res.json(userDoc);
+    } catch(e) {
+        res.status(400).json(e);
+    }
 });
 
 app.listen(4000);
--- client/mern-blog/src/pages/RegisterPage.js
+++ client/mern-blog/src/pages/RegisterPage.js
@@ -6,11 +6,16 @@ export default function RegisterPage() {
     async function register(ev) {
         // prevent default behavior of browser
         ev.preventDefault();
-        await fetch('http://localhost:4000/register', {
+        const response = await fetch('http://localhost:4000/register', {
             method: 'POST',
             body: JSON.stringify({username, password}),
             headers: {'Content-Type': 'application/json'},
         })
+        if (response.status === 200) {
+            alert('Registration successful.');
+        } else {
+            alert('Registration failed.');
+        }
     }
     return(
         <form className="register" onSubmit={register}>
