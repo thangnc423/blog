88df64a3b63faec21601697856497ad6e6ec7da3
Thang Chau <thangnc@uci.edu>
2024-11-20 16:55:08 -0800

    added checking if logged in and modifying header accordingly

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require('./models/User');
 const bcrypt = require('bcrypt');
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
-const secret = bcrypt.genSaltSync(15);
+const secret = 'b3tC9$hZ8*R5wLf@K1pX7&vG0jYq!dZm';
 
 app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://blog:qNz46U6h2QTaCrmR@cluster0.cco02.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
@@ -40,6 +42,14 @@ app.post('/login', async (req, res) => {
         res.status(400).json("Wrong login credentials.");
     }
 });
+
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    })
+})
 
 app.listen(4000);
 // mongodb+srv://blog:qNz46U6h2QTaCrmR@cluster0.cco02.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- client/mern-blog/src/Header.js
+++ client/mern-blog/src/Header.js
@@ -1,13 +1,34 @@
-import {Link} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    return (
-        <header>
-        <Link to="/" className="logo">Blog</Link>
-        <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </nav>
-        </header>
-    );
+  const [username, setUsername] = useState(null);
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUsername(userInfo.username);
+      });
+    });
+  }, []);
+  return (
+      <header>
+      <Link to="/" className="logo">Blog</Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a>Logout</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+      </header>
+  );
 }
